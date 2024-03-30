9bcd30304bc49961710a2e56c8ba6abb10651e89
Jay Potter <j@ypotter.ca>
2024-03-30 18:12:07 -0400

    new methods

--- src/Potter/Event/Dispatcher/AbstractEventDispatcher.php
+++ src/Potter/Event/Dispatcher/AbstractEventDispatcher.php
@@ -7,4 +7,7 @@ namespace Potter\Event\Dispatcher;
 abstract class AbstractEventDispatcher implements EventDispatcherInterface
 {
     abstract public function dispatch(object $event): object;
+    abstract public function getListenerProvider(): ListenerProviderInterface;
+    abstract public function hasListenerProvider(): bool;
+    abstract public function setListenerProvider(ListenerProviderInterface $listenerProvider): void;
 }
--- src/Potter/Event/Dispatcher/EventDispatcherInterface.php
+++ src/Potter/Event/Dispatcher/EventDispatcherInterface.php
@@ -4,9 +4,14 @@ declare(strict_types=1);
 
 namespace Potter\Event\Dispatcher;
 
-use \Psr\EventDispatcher\EventDispatcherInterface as PsrEventDispatcherInterface;
+use \Psr\EventDispatcher\{
+    EventDispatcherInterface as PsrEventDispatcherInterface,
+    ListenerProviderInterface
+};
 
 interface EventDispatcherInterface extends PsrEventDispatcherInterface
 {
     public function dispatch(object $event): object;
+    public function getListenerProvider(): ListenerProviderInterface;
+    public function hasListenerProvider(): bool;
 }
