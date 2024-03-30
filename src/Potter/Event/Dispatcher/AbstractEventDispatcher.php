2ae50c2160023570705e84bf15281f290557769b
Jay Potter <j@ypotter.ca>
2024-03-30 18:44:14 -0400

    protect setListenerProvider

--- src/Potter/Event/Dispatcher/AbstractEventDispatcher.php
+++ src/Potter/Event/Dispatcher/AbstractEventDispatcher.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace Potter\Event\Dispatcher;
 
+use \Psr\EventDispatcher\ListenerProviderInterface;
+
 abstract class AbstractEventDispatcher implements EventDispatcherInterface
 {
     abstract public function dispatch(object $event): object;
     abstract public function getListenerProvider(): ListenerProviderInterface;
     abstract public function hasListenerProvider(): bool;
-    abstract public function setListenerProvider(ListenerProviderInterface $listenerProvider): void;
+    abstract protected function setListenerProvider(ListenerProviderInterface $listenerProvider): void;
 }
